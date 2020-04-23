c4fd6df5944c49747069341ec3aa63fb074a9084
sumbes <p10475242@amtsybexad.onmicrosft.com>
2020-04-23 16:41:29 +0530

    jquerymoreExample

--- src/app/mydemo/jqueyimport/jqueyimport.component.ts
+++ src/app/mydemo/jqueyimport/jqueyimport.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-jqueyimport',
@@ -7,17 +10,64 @@ import * as $ from 'jquery';
 })
 export class JqueyimportComponent implements OnInit {
 
-  constructor() { 
+  constructor() {
 
-    $(document).ready(function(){
-      $("button").click(function(){
-        $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
-      });
+    $(document).ready(function () {
+
+      $('#status').keyup(function () {
+        var len = $('#status').val().length;
+
+        if (len<1){
+          $('#first').text("");
+          $('#first').removeClass("bg-warning bg-success bg-danger");
+        }
+        else if (len <= 4) {
+          $('#first').text("Weak");
+
+          $('#first').addClass("bg-danger");
+          $('#first').removeClass("bg-warning bg-success");
+        } else if (len <= 8) {
+          $('#first').text("Good");
+          $('#first').addClass("bg-warning");
+          $('#first').removeClass("bg-danger bg-success");
+        } else {
+          $('#first').text("Strong");
+          $('#first').addClass("bg-success");
+          $('#first').removeClass("bg-warning bg-danger");
+        }
+      })
+
+      var value = $('#list').val();
+      $('#bodyColorChange').css("background",value);
+
+      $('#list').change(function () {
+        var value = $('#list').val();
+        $('#bodyColorChange').css("background",value);
+
+
+      })
+
+
+      $('#liveevent').click(function () {
+        alert();
+        $(this).after("<P>Somnath Sumbe</p>");
+      })
+
+
+      
+
+      $('body').find(".soma").css('background' ,"blue");
+
+      $('li.one').each(function(){
+        $('li.one') .css('background' ,"red");
+      })
+
+
+      $('#loadfile').load("https://www.w3schools.com/html/html_filepaths.asp");
     });
-    
+
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
